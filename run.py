5fdc39f31fe26ca9348187e0c091891d06fa1ef7
Unknown <alexanderfanthome@googlemail.com>
2018-12-22 18:31:51 +0000

    Add more logs

--- run.py
+++ run.py
@@ -7,11 +7,16 @@ from train import train
 from utils.prepare_for_deployment import prepare_deploy
 
 if __name__ == '__main__':
+    print("Preparing...")
     prepare()
+    print("Training...")
     train()
+    print("Preparing to deploy...")
     prepare_deploy()
+    print("Copying files...")
     copyfile("requirements_app.txt", "_deployment/requirements.txt")
     copyfile("app.yaml", "_deployment/app.yaml")
     sh.cd("_deployment")
     print("Deploying to google...")
     sh.gcloud.app("deploy")
+    print("Done!")
