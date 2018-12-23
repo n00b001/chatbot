8d13c107ed5df28232936e8e4dfe7ba05f0f10b1
Unknown <alexanderfanthome@googlemail.com>
2018-12-23 19:48:27 +0000

    .

--- setup/settings.py
+++ setup/settings.py
@@ -51,6 +51,7 @@ preprocessing = {
     # - automatically sets number of steps and restarts training every epoch with changed learning rate
     # - ends training after set number of epochs
     # Set to None to disable
+    # 'epochs': [0.001],
     'epochs': [0.001, 0.0001, 0.00001],
 
 
